60181909c8fd0aa927df9c7fac3934e8f2a96ea0
Michael Pollett <mpollett@rmn.com>
2017-01-02 21:18:02 +0000

    slower

--- bin/automatic_clustering.sh
+++ bin/automatic_clustering.sh
@@ -1,14 +1,20 @@
 #! /bin/sh
 
 if env | egrep -q "DOCKER_RIAK_CS_AUTOMATIC_CLUSTERING=1"; then
+
+  until curl -s "http://${SEED_PORT_8080_TCP_ADDR}:8080/riak-cs/ping" | egrep "OK" > /dev/null;
+  do
+    sleep 5
+  done
+
   # Join node to the Riak and Serf clusters
-  (sleep 5; if env | egrep -q "SEED_PORT_8080_TCP_ADDR"; then
-    serf join "${SEED_PORT_8080_TCP_ADDR}" > /dev/null 2>&1
-    riak-admin cluster join "riak@${SEED_PORT_8080_TCP_ADDR}" > /dev/null 2>&1
+  (sleep 30; if env | egrep -q "SEED_PORT_8080_TCP_ADDR"; then
+    serf join "${SEED_PORT_8080_TCP_ADDR}" &> /var/log/cluster_auto
+    riak-admin cluster join "riak@${SEED_PORT_8080_TCP_ADDR}" &> /var/log/cluster_auto
   fi) &
 
   # Are we the last node to join?
-  (sleep 8; if riak-admin member-status | egrep "joining|valid" | wc -l | egrep -q "${DOCKER_RIAK_CS_CLUSTER_SIZE}"; then
-    riak-admin cluster plan > /dev/null 2>&1 && riak-admin cluster commit > /dev/null 2>&1
-  fi) &
+  (sleep 30;
+    riak-admin cluster plan &> /var/log/cluster_auto && riak-admin cluster commit &> /var/log/cluster_auto
+  ) &
 fi
